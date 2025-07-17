e6ef05f747239ee275f01aa86e6f972390ce8c70
Sudhakara H N <sudhakarahn29@gmail.com>
2025-07-17 23:41:41 +0530

    Reverse Array

---
+++ Arrays/reverseArray.js
@@ -0,0 +1,13 @@
+const reverseArray =(arr)=>{
+    let left = 0;
+    let right = arr.length-1;
+    while(left<right){
+        [arr[left],arr[right]] = [arr[right],arr[left]]
+        left ++;
+        right --;
+    }
+    return arr;
+};
+console.log(reverseArray([2,3,1,4,5,0,5]));
+// Time Complexity Big-O = O(n)
+// Space Complexity Big-O = O(1)

4b186e926d7699849e62a32465d6433ba241ef62
Sudhakara H N <sudhakarahn29@gmail.com>
2025-07-17 22:55:21 +0530

    Solved Ascending Oeder Zeros

--- Arrays/moveZerosToEndArray.js
+++ Arrays/moveZerosToEndArray.js
@@ -13,3 +13,18 @@ const moveZerosToEndArray =(arr)=>{
 console.log(moveZerosToEndArray([1,2,4,0,5,0,3]));
 //TC Big-O = O(n)
 //SC Big-O = O(1)
+
+
+// Moving Zeros Acending Order
+const moveZerosAcending= (arr) => {
+  const nonZeros = arr.filter((num)=> num!=0).sort((a,b)=>a-b);
+//   console.log(nonZeros);
+const zeros = arr.length - nonZeros.length;
+const finalZeros = Array(zeros).fill(0);
+// console.log(finalZeros);
+return  [...finalZeros,...nonZeros]
+};
+console.log(moveZerosAcending([1, 2, 4, 0, 5, 0, 3]));
+
+//TC Big-O = O(n + k log k) → Worst case: O(n log n)
+//SC Big-O = 	O(n)

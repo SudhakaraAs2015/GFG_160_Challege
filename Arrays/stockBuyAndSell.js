76b07a7bf645054b8be50e8e8dc1595f31e834bf
Sudhakara H N <sudhakarahn29@gmail.com>
2025-07-29 23:00:34 +0530

    Stock Buy and Sell

---
+++ Arrays/stockBuyAndSell.js
@@ -0,0 +1,14 @@
+function stockBuyAndSell(prices) {
+  let profit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      profit += prices[i] - prices[i - 1];
+    }
+  }
+  return profit;
+}
+const prices = [100, 180, 260, 310, 40, 535, 695];
+console.log(stockBuyAndSell(prices));
+// TC =O(n)
+// SC = O(1)
